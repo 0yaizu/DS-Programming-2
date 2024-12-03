b15984b708e668a5081cfbd9cf7ce9f910745a14
0yaizu <kaira.oyaizu@gmail.com>
2024-12-03 10:41:50 +0900

    remake route change.

--- weather/main.py
+++ weather/main.py
@@ -1,7 +1,14 @@
 import flet as ft
 
-
 def main(page: ft.Page):
+
+	def change_page(pageid: int):
+		if pageid == 0:
+			page.go("/home")
+		elif pageid == 1:
+			page.go("/search")
+		elif pageid == 2:
+			page.go("/view2")
 
 	custom_color_scheme = ft.ColorScheme(
 		primary='#141417',
@@ -27,12 +34,14 @@ def main(page: ft.Page):
       ft.NavigationDestination(icon=ft.icons.SEARCH, label="検索"),
       ft.NavigationDestination(icon=ft.icons.ACCOUNT_CIRCLE, label="アカウント"),
     ],
-    on_change=lambda e: print(f"選択されたインデックス: {e.control.selected_index}"),
+		offset=ft.transform.Offset(0, 0),
+		animate_offset=ft.animation.Animation(0),
+    on_change=lambda e: change_page(e.control.selected_index),
 		bgcolor=custom_color_scheme.background,
   )
 
 	def create_home():
-		return ft.View("/home", [
+		return [
 			ft.AppBar(
 				title=ft.Text("Home"),
 				# bgcolor=ft.colors.BLUE
@@ -56,22 +65,20 @@ def main(page: ft.Page):
 				expand=True,
 			),
 			navigation_bar,
-		],
-		bgcolor=custom_color_scheme.background,
-		)
+		]
 
 	def create_view2():
-		return ft.View("/view2", [
+		return [
 			ft.AppBar(
 				title=ft.Text("view2"),
 			),
 			ft.TextField(value="view2"),
 			ft.ElevatedButton(
 				"Go to home", on_click=lambda _: page.go("/home")),
-		])
+		]
 	
 	def create_404():
-		return ft.View("/page_not_found", [
+		return [
 			ft.AppBar(
 				title=ft.Text("Page not found!"),
 				bgcolor=ft.colors.BLUE),
@@ -93,7 +100,31 @@ def main(page: ft.Page):
         vertical_alignment=ft.CrossAxisAlignment.CENTER,
 			),
 			navigation_bar,
-		])
+		]
+
+	def create_search():
+		return [
+			ft.AppBar(
+				title=ft.Text("Search"),
+				bgcolor=ft.colors.BLUE),
+			ft.Row(
+				controls=[
+					ft.Column(
+						controls=[
+							ft.Text("Search", size=50),
+							ft.ElevatedButton(
+								"Go back to home", on_click=lambda _: page.go("/home")
+							),
+						],
+						alignment=ft.MainAxisAlignment.CENTER,
+						horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+						expand=True,
+					),
+				],
+				expand=True,
+			),
+			navigation_bar,
+		]
 
 	# ルート
 	def route_change(handler):
@@ -101,13 +132,14 @@ def main(page: ft.Page):
 			"/home": create_home(),
 			"/page_not_found": create_404(),
 			"/view2": create_view2(),
+			"/search": create_search(),
 		}
-		page.views.clear()
+		page.controls.clear()
 		try:
-			page.views.append(routes[handler.route])
+			page.add(*routes[handler.route][0:])
 		except:
-			page.views.append(routes["/page_not_found"])
-		page.update()
+			page.add(*routes["/page_not_found"][0:])
+		# page.update()
 
 	# ルート変更時のロジック設定
 	page.on_route_change = route_change
